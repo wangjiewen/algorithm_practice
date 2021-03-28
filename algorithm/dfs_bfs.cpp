d182e3aa355dd6161e34c2d336057ae77d6176e8
JieWen Wang <issinfonia@gmail.com>
2021-03-28 12:36:41 +0800

    dfs & bfs 思路

---
+++ algorithm/dfs_bfs.cpp
@@ -0,0 +1,25 @@
+
+dfs(region, location) {
+	if (location ok) {
+		mark location;
+		dfs(regin, next location);
+		dfs(regin, next location);
+		...
+		return conditions are met;
+	}
+	return conditions aren't meet;
+}
+
+bfs()
+{
+	queue;
+	while (queue isn't empty) {
+		take one form queue;
+		if (location ok) {
+			mark or change;
+			add next location to queue;
+			add next location to queue;
+			...
+		}
+	}
+}
