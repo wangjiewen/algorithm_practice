d3c25a155dc23c53be00c90c0cda883789a32b89
JieWen Wang <issinfonia@gmail.com>
2022-06-16 00:03:38 +0800

    cf config

---
+++ .cf/template/cpp.cpp
@@ -0,0 +1,26 @@
+/*
+ * No hard feelings.
+ * Author: $%U%$
+ * Time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+}
+
+int main(void)
+{
+	int loop;
+	scanf("%d", &loop);
+	while (loop--) {
+		solve();
+	}
+
+	return 0;
+}
+
